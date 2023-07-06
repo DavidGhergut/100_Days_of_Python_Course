627ef6a4dcaa463631f44661f9271077c603ad17
David <56410527+DavidGhergut@users.noreply.github.com>
2023-07-06 14:43:10 +0200

    Add Rock Paper Scissors start project

---
+++ rock-paper-scissors-start.py
@@ -0,0 +1,56 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper and 2 for Scissors."))
+
+if user_choice == 0:
+  print(rock)
+elif user_choice == 1:
+  print(paper)
+else:
+  print(scissors)
+
+print("The computer chose:")
+computer_choice = random.randint(0, 2);
+
+if computer_choice == 0:
+  print(rock)
+elif computer_choice == 1:
+  print(paper)
+else:
+  print(scissors)
+
+if user_choice == computer_choice:
+  print("Draw!")
+elif (user_choice == 0 and computer_choice == 2) or (user_choice == 1 and computer_choice == 0) or (user_choice == 2 and computer_choice == 1):
+  print("You win 🎉")
+else:
+  print("You lose 💀")
